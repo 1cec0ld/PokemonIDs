ffc96e8518e1fd5bb24f4cefcd81c952cc990c2f
Jeremy Luchak <jeremy@nevada.unr.edu>
2019-08-05 20:40:21 -0700

    less elegant fix for the bug that wont die

--- src/main/java/com/gmail/ak1cec0ld/plugins/pokemonserver/hms/fly/FlyController.java
+++ src/main/java/com/gmail/ak1cec0ld/plugins/pokemonserver/hms/fly/FlyController.java
@@ -78,11 +78,15 @@ public class FlyController {
     private Advancement getAdvancement(String flypoint){
         String[] regions = new String[] {"kanto","johto","hoenn", "sinnoh", "unova", "kalos", "alola", "galar"};
         NamespacedKey nsk;
-        for(String each : regions){
-            nsk = new NamespacedKey(getPlugin(), "places/"+each.toLowerCase().replaceAll("[^a-z0-9]","")+"/"+flypoint);
-            if(Bukkit.getServer().getAdvancement(nsk) != null){
-                return Bukkit.getServer().getAdvancement(nsk);
+        try {
+            for (String each : regions) {
+                nsk = new NamespacedKey(getPlugin(), "places/" + each.toLowerCase() + "/" + flypoint);
+                if (Bukkit.getServer().getAdvancement(nsk) != null) {
+                    return Bukkit.getServer().getAdvancement(nsk);
+                }
             }
+        } catch(IllegalArgumentException e) {
+            return null;
         }
         return null;
     }
